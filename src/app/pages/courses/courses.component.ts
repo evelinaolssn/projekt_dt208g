167c930f4e55f5e7fe3fe81f3728c9d63dd3c350
Evelina Olsson <evol2401@student.miun.se>
2025-10-16 13:24:53 +0200

    Added search bar and filtering functionality of course code and course name

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Course } from '../../course';
 import { CourseService } from '../../services/course.service';
+import { FormsModule } from '@angular/forms';
 
 /**
  * Component for displaying courses from JSON data
@@ -10,12 +11,13 @@ import { CourseService } from '../../services/course.service';
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.html',
   styleUrl: './courses.css'
 })
 export class CoursesComponent implements OnInit {
   courses: Course[] = [];
+  searchField: string = '';
 
   /**
    * Connects the component to CourseService
@@ -48,5 +50,17 @@ export class CoursesComponent implements OnInit {
   /** Sorts data by course subject */
   sortBySubject(): void {
     this.courses.sort((a, b) => a.subject.localeCompare(b.subject));
+  }
+
+  /**
+   * Filters courses based on searchinput
+   * Matches if input is found in course code or course name
+   * @returns Filtered array of courses
+   */
+  get filteredCourses(): Course[] {
+    return this.courses.filter(course =>
+      course.courseCode.toLowerCase().includes(this.searchField.toLowerCase()) ||
+      course.courseName.toLowerCase().includes(this.searchField.toLowerCase())
+    );
   }
 }
