0a42e5b6e1b7469d1678f72e58a67561770dc973
Evelina Olsson <evol2401@student.miun.se>
2025-10-20 17:31:30 +0200

    Added course list rendering in StudyPlanComponent

--- src/app/pages/study-plan/study-plan.component.ts
+++ src/app/pages/study-plan/study-plan.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { StudyPlanService } from '../../services/study-plan.service';
+import { Course } from '../../course';
 @Component({
   selector: 'app-study-plan',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './study-plan.html',
   styleUrl: './study-plan.css'
 })
 export class StudyPlanComponent {
 
+  /**
+   * Connects the component to studyPlanService
+   * @param studyPlanService Service that manage the study plan
+   */
+  constructor(private studyPlanService: StudyPlanService) { }
+
+  /**
+   * Retrieves courses from study plan service
+   * @returns {Course[]} Array of courses in study plan
+   */
+  get studyPlan(): Course[] {
+    return this.studyPlanService.getStudyPlan();
+  }
 }
